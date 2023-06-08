c484981baefed773634d4025d7d41a49244c6f08
AESHAH AHMED ALSAGGAF <aisha.alsaggaf13@gmail.com>
2023-06-08 18:47:58 +0300

    Add route for cars

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'cars/index'
+  resources :cars, only: [:index] #URL /cars 
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
