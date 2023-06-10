60385fc64efb1e560892c9abad5fc39e4d5509b6
AESHAH AHMED ALSAGGAF <aisha.alsaggaf13@gmail.com>
2023-06-10 18:19:59 +0300

    Add route to details page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "cars#index"
-  
+  get "/details" , to: "details#show"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
